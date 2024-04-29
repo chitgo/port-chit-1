66875e4a04ccde18ddf1a151f6e7b98c3721f74b
chitgo <kostas.chitos@gmail.com>
2024-04-30 02:23:06 +0300

    z-index

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ function Navbar() {
     return (
       <ul
         key={content}
-        className="flex flex-col items-center gap-10 sm:gap-4 sm:flex-row"
+        className='flex flex-col items-center gap-10 sm:gap-4 sm:flex-row'
       >
         <li
           className={`px-2 mt-8 text-4xl font-bold transition duration-300 sm:text-2xl hover:text-blue-500 sm:mt-0 ${
@@ -60,19 +60,19 @@ function Navbar() {
   }
 
   return (
-    <section className="sticky top-0 flex items-center justify-between px-4 mx-auto overflow-hidden bg-white sm:px-8 md:px-12 lg:px-20 2xl:px-40 py-7 text-secondary">
-      <a onClick={handleLogoClick} className="cursor-pointer">
-        <h2 className="text-3xl font-bold ">Chit.dev</h2>
+    <section className='sticky top-0 z-10 flex items-center justify-between px-4 mx-auto overflow-hidden bg-white sm:px-8 md:px-12 lg:px-20 2xl:px-40 py-7 text-secondary'>
+      <a onClick={handleLogoClick} className='cursor-pointer'>
+        <h2 className='text-3xl font-bold '>Chit.dev</h2>
       </a>
 
-      <div className="flex items-center ">
+      <div className='flex items-center '>
         <div
           onClick={handleMenu}
-          className="transition duration-300 cursor-pointer sm:hidden hover:text-primary"
+          className='transition duration-300 cursor-pointer sm:hidden hover:text-primary'
         >
-          <AiOutlineMenu className="text-2xl " />
+          <AiOutlineMenu className='text-2xl ' />
         </div>
-        <div className="hidden sm:flex">
+        <div className='hidden sm:flex'>
           {navLinks.map((nav) => renderNavLink(nav))}
         </div>
       </div>
@@ -80,14 +80,14 @@ function Navbar() {
       <AnimatePresence>
         {isOpen && (
           <motion.div
-            className="fixed top-0 left-0 z-10 w-screen h-screen transition duration-1000 bg-white sm:hidden"
+            className='fixed top-0 left-0 z-10 w-screen h-screen transition duration-1000 bg-white sm:hidden'
             initial={{ opacity: 0, x: '-100%' }}
             animate={{ opacity: 1, x: 0 }}
             exit={{ opacity: 0, x: '-100%' }}
           >
-            <div className="flex flex-col gap-8 mt-24">
+            <div className='flex flex-col gap-8 mt-24'>
               <AiOutlineClose
-                className="absolute text-2xl transition duration-300 cursor-pointer right-8 top-9 hover:text-primary"
+                className='absolute text-2xl transition duration-300 cursor-pointer right-8 top-9 hover:text-primary'
                 onClick={handleMenu}
               />
               {navLinks.map((nav) => renderNavLink(nav))}
